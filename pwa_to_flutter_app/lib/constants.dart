b917139d001a14b7db5d694cc3c8783803b40dd7
Cihan Barış Yüksel <chnbrs@google.com>
2023-03-15 18:33:06 +0300

    save

--- pwa_to_flutter_app/lib/constants.dart
+++ pwa_to_flutter_app/lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 // PWA Url
-final kPwaUri = WebUri('https://mdn.github.io/pwa-examples/js13kpwa/');
+final kPwaUri = WebUri('https://gulsahinciftligi.com');
 final kPwaHost = kPwaUri.host;
 
 // Custom HTML Error Page.
@@ -9,7 +9,7 @@ const kHTMLErrorPage = """
 <html>
   <head></head>
   <body>
-    <h1>Not connected to Internet :(</h1>
+    <h1>İnternet Bağlantınızı kontrol ediniz.</h1>
   </body>
 </html>
 """;
@@ -19,7 +19,7 @@ const kHTMLErrorPageNotInstalled = """
 <html>
   <head></head>
   <body>
-    <h1>You must be connected to Internet at least one time to install correctly the App.</h1>
+    <h1>İnternete bağlı olmalısınız.</h1>
   </body>
 </html>
 """;
